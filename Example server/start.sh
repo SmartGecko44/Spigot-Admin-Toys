25d705ddd34cf288f512297edfa5f20055545bfa
smartgecko44 <74975165+SmartGecko44@users.noreply.github.com>
2023-12-28 02:44:36 +0100

    Added code quality checking

--- Example server/start.sh
+++ Example server/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cd "$( dirname "$0" )"
+cd "$( dirname "$0" )" || exit
+# "-Xms1G" is the minimum amount of ram the server is allowed to use (In this case 1GB of RAM
+# "-Xmx6G" is the maximum amount of ram the server is allowed to use (In this case 6GB of RAM
+# Change these values according to your needs
 java -Xms1G -Xmx6G -XX:+UseG1GC -jar spigot-1.12.2.jar nogui
