445670d7f9215b5e45786b4b7a9ce78b0f2e5ce1
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2024-05-19 13:43:17 +0200

    Fixed an issue with the hashCode function in Aim.java and made the enchantment process more modular

--- src/main/java/org/gecko/spigotadmintoys/Main.java
+++ src/main/java/org/gecko/spigotadmintoys/Main.java
@@ -5,10 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.gecko.spigotadmintoys.data.ConfigurationManager;
-import org.gecko.spigotadmintoys.enchantments.enchants.weapons.bows.*;
-import org.gecko.spigotadmintoys.enchantments.enchants.weapons.swords.Disarm;
-import org.gecko.spigotadmintoys.enchantments.tools.pickaxes.Drill;
-import org.gecko.spigotadmintoys.enchantments.tools.pickaxes.Smelt;
+import org.gecko.spigotadmintoys.enchantments.logic.EnchantmentHandler;
 import org.gecko.spigotadmintoys.gui.ConfigGUI;
 import org.gecko.spigotadmintoys.logic.SetAndGet;
 import org.gecko.spigotadmintoys.startup.Register;
@@ -43,7 +40,7 @@ public final class Main extends JavaPlugin {
             }
         }
         // It's been registered!
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + enchantment.getName() + " Registered");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + enchantment.getName() + " with ID " + enchantment.hashCode() + " registered");
     }
 
     @Override
@@ -70,14 +67,14 @@ public final class Main extends JavaPlugin {
 
         // Register Enchantments
         try {
-            registerEnchantment(new Disarm()); // Id: 100
-            registerEnchantment(new Aim()); // Id: 101
-            registerEnchantment(new Multishot()); // Id: 102
-            registerEnchantment(new Drill()); // Id: 103
-            registerEnchantment(new Smelt()); // Id: 104
-            registerEnchantment(new Glow()); // Id: 105
-            registerEnchantment(new Endanger()); // Id: 106
-            registerEnchantment(new Explosive());// Id: 107
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(0)); // Id: 100
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(1)); // Id: 101
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(2)); // Id: 102
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(3)); // Id: 103
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(4)); // Id: 104
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(5)); // Id: 105
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(6)); // Id: 106
+            registerEnchantment(EnchantmentHandler.getAllEnchantments().get(7));// Id: 107
         } catch (IllegalArgumentException | RegisterError ignored) {
             // Ignore any exceptions during enchantment registration
         }
